e51faf339124f8c28dfc2ff3ab6b71f326198d46
Muhammad Faahiz cc <ccfaahiz@gmail.com>
2025-04-28 13:02:59 +0530

    added update & delete

--- delivery/urls.py
+++ delivery/urls.py
@@ -7,9 +7,18 @@ urlpatterns = [
     path('signup/', views.signup),
     path('login/', views.handle_login,name='handle_login'),
     path('sign_up/', views.handle_signup,name='handle_signup'),
+    
     path('login/restaurant/', views.restaurant_page,name='restaurant_page'),
     path('login/restaurants/', views.add_restaurant,name='add_restaurant'),
     path('login/show_restaurant_page/', views.show_restaurant_page,name='show_restaurant_page'),
     path('restaurant/<int:restaurant_id>/menu', views.restaurant_menu, name='restaurant_menu'),
+    path('restaurants/<int:restaurant_id>/update/', views.update_restaurant, name='update_restaurant'),
+    path('restaurants/<int:restaurant_id>/update/page/', views.update_restaurant_page, name='update_restaurant_page'),
+    path('restaurants/<int:restaurant_id>/delete/', views.delete_restaurant, name='delete_restaurant'),
+    
+    path('menu/<int:menuItem_id>/update/', views.update_menuItem, name='update_menuItem'),
+    path('menu/<int:menuItem_id>/update/page/', views.update_menuItem_page, name='update_menuItem_page'),
+    path('menu/<int:menuItem_id>/delete/', views.delete_menuItem, name='delete_menuItem'),
+
     
 ]
--- delivery/views.py
+++ delivery/views.py
@@ -97,3 +97,59 @@ def restaurant_menu(request, restaurant_id):
     menu_items = restaurant.menu_items.all()
 
     return render(request, 'delivery/menu.html', {'restaurant':restaurant, 'menu_items': menu_items,})
+
+# Update Restaurant Page
+def update_restaurant_page(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    return render(request, 'delivery/update_restaurant_page.html', {"restaurant":restaurant})
+
+# Update Restaurant
+def update_restaurant(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+
+    if request.method == "POST":
+        restaurant.name = request.POST.get('name')
+        restaurant.picture = request.POST.get('picture')
+        restaurant.cuisine = request.POST.get('cuisine')
+        restaurant.rating = request.POST.get('rating')
+        restaurant.save()
+
+        restaurants = Restaurant.objects.all()
+        return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
+    
+# Delete Restaurant
+def delete_restaurant(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    restaurant.delete()
+
+    restaurants = Restaurant.objects.all()
+    return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
+
+# Update MenuItem Page
+def update_menuItem_page(request, menuItem_id):
+    menuItem = get_object_or_404(MenuItem, id=menuItem_id)
+    return render(request, 'delivery/update_menuItem_page.html', {"item":menuItem})
+
+
+# Update MenuItem
+def update_menuItem(request, menuItem_id):
+    menuItem = get_object_or_404(MenuItem, id=menuItem_id)
+
+    if request.method == "POST":
+        menuItem.name = request.POST.get('name')
+        menuItem.description = request.POST.get('description')
+        menuItem.price = request.POST.get('price')
+        menuItem.is_veg = request.POST.get('is_veg') == 'on'
+        menuItem.picture = request.POST.get('picture')
+        menuItem.save()
+
+        restaurants = Restaurant.objects.all()
+        return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
+    
+# Delete MenuItem
+def delete_menuItem(request, menuItem_id):
+    menuItem = get_object_or_404(MenuItem, id=menuItem_id)
+    menuItem.delete()
+
+    restaurants = Restaurant.objects.all()
+    return render(request, 'delivery/show_restaurants.html', {"restaurants": restaurants})
